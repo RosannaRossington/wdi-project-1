56f07d34238e70a44d924d3f0e00ea8a2f219cb5
RosannaRossington <rosanna_rossington@outlook.com>
2016-03-24 20:18:13 +0000

    logging button that has been clicked

--- js/app.js
+++ js/app.js
@@ -1,17 +1,28 @@
-console.log ("rosie");
-document.addEventListener("DOMContentLoaded",start); 
+//  preventDefault()
+document.addEventListener("DOMContentLoaded",play); 
 
 var buttons = document.getElementsByClassName("buttons");
+var playerOne = [];
+var playerTwo = [];
 
 //go through each button and listen for a click
-//if clicked get next round
-  function start (){
+//if clicked begin next round
+//if longer than 3 seconds begin next round
+//get the id of the element that fired the click
+  function play (){
     for (var i=0;i<buttons.length;i++){
-      buttons[i].addEventListener("click", nextRound);
+      buttons[i].addEventListener("click", calculatePoints); 
     }
   }
 
-//
-  function nextRound (){
-  console.log("clicked")  
+
+//update the box and buttons
+  function calculatePoints (){
+    var button = event.target.id;
+    console.log (button);
   }
+
+// //take final scores and return who wins  
+//   function getWinner(playerOne,playerTwo){
+  
+//   }
