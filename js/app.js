a616a526eff79644d4feab7e97c20207257c2200
RosannaRossington <rosanna_rossington@outlook.com>
2016-03-24 19:33:31 +0000

    added buttons to create the nextRound function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 console.log ("rosie");
+document.addEventListener("DOMContentLoaded",start); 
+
+var buttons = document.getElementsByClassName("buttons");
+
+//go through each button and listen for a click
+//if clicked get next round
+  function start (){
+    for (var i=0;i<buttons.length;i++){
+      buttons[i].addEventListener("click", nextRound);
+    }
+  }
+
+//
+  function nextRound (){
+  console.log("clicked")  
+  }
