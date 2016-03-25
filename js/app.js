05a4017ac37cecabe0b95989497ff9f201e9d353
RosannaRossington <rosanna_rossington@outlook.com>
2016-03-25 12:59:57 +0000

    change of plan! No longer looping different rounds instead logging random text in the square

--- js/app.js
+++ js/app.js
@@ -1,61 +1,51 @@
-//  preventDefault()
-document.addEventListener("DOMContentLoaded",start); 
+        document.addEventListener("DOMContentLoaded",start); 
 
-var buttons = document.getElementsByClassName("buttons");
+        var buttons = document.getElementsByClassName("buttons");
 
-//go through each button and listen for a click
-
-  function start (){
-    for (var i=0;i<buttons.length;i++){
-      buttons[i].addEventListener("click", play); 
-    }
-  }
+        function start (){
+            for (var i=0;i<buttons.length;i++){
+              buttons[i].addEventListener("click", play); 
+            }
+        }
 
 //on click log score and start next round
 //get the id of the element that fired the click & compare to box id
 //if clicked begin next round
 //if longer than 3 seconds begin next round
-  
-  function nextRound(){
-    
-  }
+        function play (){
+          var colors = ["red","green","blue","pink","purple","yellow"]
+          var colorSelection = colors[Math.floor(Math.random()*colors.length)];
+          var textSelection = colors[Math.floor(Math.random()*colors.length)];
+          var box = document.querySelector("li");
+          
+          //apply colorSelection as a font color to text in box
+          //apply textSelection as the text in the box
+          box.innerHTML = textSelection;
+          console.log (box)
+          
+          //apply colorSelection and textSelection to each button
+        
+        }    
+         
+         
 
-  function play (button,box){
-    var button = event.target.value;
-    var box = document.querySelector("li").id;
-    var playerScore = 0;
-    
-    console.log (button);
-    console.log (box);
-  
-    if (button === box) {
-      playerScore++;
-        nextRound();
-    }
-    else if (button !== box) {
-      playerScore;
-       nextRound();
-    }
-    console.log (playerScore);
-  }
+        //   // var box = document.querySelector("li").id;
+        //   var button = event.target.value;
+          
+        //   var playerScore = 0;
+          
+        //   console.log (button);
+        //   console.log (box);
+        
+        //   if (button === box) {
+        //     playerScore++;
+        //       nextRound();
+        //   }
+        //   else if (button !== box) {
+        //     playerScore;
+        //      nextRound();
+        //   }
+        //   console.log (playerScore);
+        // }
 
-//think twitter append - update html and then slide off
-  // function nextRound (){
-  //    var button1Update =
-  //    var button2Update = 
-  //    var boxUpdate =
-  //    var game = <section class="box">
-      //   <li id="black">+White+</li>    
-      //   </section>
-      //   <section class="buttons">
-      //   <div><input type="button" value="WHITE" id="button1"></div>
-      //   <div><input type="button" value="BLACK"id="button2"></div>
-      //   </section>
-  // }
 
-// //take final scores and return who wins  
-//   function getWinner(playerOne,playerTwo){
-  // var score = document.querySelector("p");
-  // score.innerHTML = "Player 1 scores " + playerScore + "."
-  // };
-//   }
