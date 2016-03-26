451a26f2aafd9384e1cd221976aea9d93c5b46ee
RosannaRossington <rosanna_rossington@outlook.com>
2016-03-26 11:40:58 +0000

    correcting as buttons not offering correct answer

--- js/app.js
+++ js/app.js
@@ -3,16 +3,16 @@
         var buttons = document.getElementsByClassName("buttons");
         var start = document.getElementById("Start");
 
-        var player1Score = 0;
-        var player2Score = 0;
 
       function start(){
-        //  start.addEventListener("click", createRound); 
+//  start.addEventListener("click", play);} 
           for (var i=0;i<buttons.length;i++){
-              buttons[i].addEventListener("click", play); 
+              buttons[i].addEventListener("click", createRound); 
           } 
+      }
 
-            function play (){
+      function createRound (){
+
                 var colors = ["red","green","blue","pink","purple","yellow"]
                 var colorSelection = colors[Math.floor(Math.random()*colors.length)];
                 var textSelection = colors[Math.floor(Math.random()*colors.length)];
@@ -21,47 +21,104 @@
 
                 var button1 = document.getElementById("Button1");
                 var button2 = document.getElementById("Button2");
-      //apply colorSelection as a font color to text in box
-      //apply textSelection as the text in the box          
+//apply colorSelection as a font color to text in box - apply textSelection as the text in the box          
                 box.innerHTML = textSelection;
                 box.style.color = colorSelection;
 
-      //apply both as the buttons but RANDOMLY
-      //buttons can't be the same!
-              function createButton() {
+
+//apply both as the buttons but RANDOMLY - buttons can't be the same!
+              function createButtons() {
                     var randomNumber = Math.random();
-                    if (randomNumber <= 0.5) {
+                    if (randomNumber < 0.5) {
                         return "button1";
                     }  else {
                         return "button2";
                     }
               }
-                    if ((createButton() === "button1") && (textSelection !== colorSelection)) {
-                      button1.value = textSelection;
-                      button2.value = colorSelection;
-                    } else if ((createButton() === "button2") && (textSelection !== colorSelection)){
-                      button2.value = textSelection;
-                      button1.value = colorSelection;
-                    } else createButton();
-            }
-      }   
+                  if (createButtons() === "button1")  {
+                      button1.value = box.innerHTML;
+                      button2.value = box.style.color;
+                    } else {(createButtons() === "button2")
+                      button2.value = box.innerHTML;
+                      button1.value = box.style.color;
+                    } 
+      } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // if (event.target.value === box.style.color) {
+        //           player1Score +=1
+        //           console.log (player1Score)
+        //          }
+        // else if (event.target.value !== box.style.color) {
+        //            player1Score
+        //            console.log (player1Score)
+                        
+        //}       
+            
       
         
 
          //if button not pressed after 3 seconds        
-                 // $(function timeUp(){
-                 //  $('buttons').on("click",function() {
-                 //    ($(this).slideUp("game"));
-                 //  });
+                 // $(function (){
+                 // $('.game').on("click",function() {
+                 // ($(this).slideUp("game"));
+                 // });
                  // });  
                  // timeUp()   
                
+        //CLICK SLIDE UP - nothing back to bottom.
+               // $(function(){
+               //   $('li').on("click",function() {
+               //     ($(this).slideUp("class"));
+               //   });
+               // });
+
         //play on loop up to playerScore 5 - when button clicked OR if 3 seconds up
         //winning combo is value of button clicked === box.style.color  
         //log point1 and play again
         //play 5 times   
               
               // function keepScore(){
+              //   var player1Score = 0;
               //     while (player1Score <= 5) {
               //        if (event.target.value === box.style.color) {
               //           player1Score +=1
@@ -70,6 +127,7 @@
               //            player1Score
               //         }
               //       } 
+              // }      
 
 
 
