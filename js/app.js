246896f00e88c384b8f7bedb0dffae509b0b19b6
RosannaRossington <rosanna_rossington@outlook.com>
2016-03-26 22:40:34 +0000

    start button replaced with game

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
         document.addEventListener("DOMContentLoaded",start); 
+        $(function(){
+                    $("#game").hide();});
 
         var buttons = document.getElementsByClassName("buttons");
         var start = document.getElementById("Start");
         var game = document.getElementById("game");
+       
 
 //when start is clicked the first round will begin and remain on the screen for 3 seconds
       function start(){
@@ -10,6 +13,7 @@
          $(function(){
             $("#Start").on("click",function() {
               ($(this).remove("#Start"));
+              ($("#game").show());
               setInterval(createRound,3000);  
           });
         });
@@ -17,9 +21,10 @@
 //each time a button is clicked the score will be updated
       function createRound (){
         for (var i=0;i<buttons.length;i++){
-         buttons[i].addEventListener("click",keepScore);
+         buttons[i].addEventListener("click",keepScore);{
+         }
         }
-
+      
         var colors = ["red","green","blue","pink","purple","yellow"]
         var colorSelection = colors[Math.floor(Math.random()*colors.length)];
         var textSelection = colors[Math.floor(Math.random()*colors.length)];
@@ -49,19 +54,41 @@
                     } 
 
               function keepScore() {
+                var roundCounter = document.getElementById("roundsPlayed");
                 var playerCounter = document.getElementById("playerScore");
+               
+                var roundUp = document.querySelector("p")
 
-                  if ((event.target).value === box.style.color) 
-                            {playerCounter.innerHTML++
+                  if ((event.target).value === box.style.color){
+                      playerCounter.innerHTML++;
+                      roundCounter.innerHTML++;
                   }else if ((event.target).value !== box.style.color) {
-                          playerCounter.innerHTML}    
+                      playerCounter.innerHTML;
+                      roundCounter.innerHTML++}   
               }
+
+
           
       } 
  
             // loop through for 10 and then clearInterval()
   
-        
+  //when playerCounter === 5 the game will disappear
+
+  // $(document).ready(function()
+  //  {
+  //    $("#buttons").click(function(e)
+  //     {
+  //      var subject = $("#game");
+
+  //      if (e.target.id != subject.attr('id'))
+  //        {
+  //            subject.fadeOut();
+  //        }
+  //     });
+  //  });
+
+        //roundUp.innerHTML = "You played"+roundCounter + "scored"+playerCounter+"."
         
  
         
