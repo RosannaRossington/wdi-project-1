6e4054fa06f2193619081f17b130690726ebf63e
RosannaRossington <rosanna_rossington@outlook.com>
2016-03-28 20:12:53 +0100

    logging score at the end

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded",start);
   // var start = document.getElementById("Start");
   var game = document.getElementById("game");
   var levelSelector = document.getElementById("levelSelector");
+  var roundCounter = 0;
+  var playerCounter = 0;
      
 function start(){
     $("#game").hide();
@@ -61,7 +63,7 @@ function setLevel(){
       $("#firstTitle").show();
       $("#game").show();
       setInterval(createRound,2500);
-      setTimeout(function(){endGame()},25000)
+      // setTimeout(function(){endGame()},25000)
     } else if (((this).id) === ("LevelTwo")) {
       $("#secondTitle").show();
       $("#game").show();
@@ -79,7 +81,6 @@ function setLevel(){
 }         
 
 function createRound(level){
-
   var colors = ["red","green","blue","pink","purple","yellow","orange","black","turquoise","brown"]
   var colorSelection = colors[Math.floor(Math.random()*colors.length)];
   var textSelection = colors[Math.floor(Math.random()*colors.length)];
@@ -90,6 +91,19 @@ function createRound(level){
   var box = document.querySelector("li");
   var button1 = document.getElementById("Button1");
   var button2 = document.getElementById("Button2");
+
+  var roundCounterElement = document.getElementById("roundsPlayed");
+  var playerCounterElement = document.getElementById("playerScore");
+  
+  var box = document.querySelector("li");
+
+             
+  if (roundCounter < 10){ 
+    console.log ("this is round " + roundCounter) 
+    roundCounter++;
+  } else {
+    endGame(roundCounter,playerCounter) 
+  }
 //apply colorSelection as a font color to text in box - apply textSelection as the text in the box    
 // console.log (boxColor)      
     if (level === "LevelFour")  {
@@ -112,11 +126,14 @@ function createRound(level){
 //logging a round even if not clicked but point also 
 //if element clicked score logged 
    $('.button').each(function(index, element) {
- 
-      $(element).one("click", keepScore)
- 
+      $(element).off("click");
+      $(element).one("click", function() {
+        keepScore(box);
+      });
     });
-
+   roundCounterElement.innerHTML = roundCounter;
+   playerCounterElement.innerHTML = playerCounter;
+   console.log (playerCounter,roundCounter);
 //buttons to not be the same
 // function assignButtons () {
 //        if (createButtons() === "button1"){
@@ -151,64 +168,28 @@ function createButtons() {
         return "button2";
     }
 }
-                  
-function keepScore() {
-  var roundCounterElement = document.getElementById("roundsPlayed");
-  var playerCounterElement = document.getElementById("playerScore");
-  
-  var roundCounter = parseInt(roundCounterElement.innerHTML);
-  var playerCounter = parseInt(playerCounterElement.innerHTML);
-  var box = document.querySelector("li");
 
-  console.log (playerCounter,roundCounter) 
-             
-  if (roundCounter < 10){ console.log ("this is round" + roundCounter) 
-    if ((event.target).value === box.style.color){
-        playerCounter++;   
-    } 
-    roundCounter++; 
-  } else { 
-    endGame(roundCounter,playerCounter) 
-    } 
-    roundCounterElement.innerHTML = roundCounter;
-    playerCounterElement.innerHTML = playerCounter;
+function keepScore(box) {
+  if ((event.target).value == box.style.color){
+    playerCounter++;   
+    console.log(playerCounter)
+  } 
 }
 
 function endGame(rounds,user) {
   var gameOver = document.querySelector("p");
-  var playAgain = document.getElementById("PlayAgain");
+  var play = document.getElementById("PlayAgain");
+    $("#game").hide();
+    $("#score").show();
 
-  PlayAgain.addEventListener("click", selectLevel);
+  play.addEventListener("click", selectLevel);
   // $('#PlayAgain').one ()
   // $("#PlayAgain").click(selectLevel); 
 
-    $("#game").hide();
-      clearInterval();
-    $("#score").show();
-      $("#score").animate({left: '250px'});
+    
   
   gameOver.innerHTML = "You played "+ rounds + " scored "+ user +" ."     
 }
-
-
-
-
-//original
-// function endGame(rounds,user) {
-//   var gameOver = document.querySelector("p");
-//   var playAgain = document.getElementById("PlayAgain");
-
-//   PlayAgain.addEventListener("click", selectLevel);
-//   // $('#PlayAgain').one ()
-//   // $("#PlayAgain").click(selectLevel); 
-
-//     $("#game").hide();
-//       clearInterval();
-//     $("#score").show();
-//       $("#score").animate({left: '250px'});
-  
-//   gameOver.innerHTML = "You played "+ rounds + " scored "+ user +" ."     
-// }
 
         
         
